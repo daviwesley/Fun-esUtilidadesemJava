86feee8d75cc6ccaaa0ae06a704bb21c87af402d
Davi Wesley <davi.wesley@icloud.com>
2016-12-09 17:47:31 -0200

    Update scaneardata.java

--- scaneardata.java
+++ scaneardata.java
@@ -8,30 +8,28 @@ import java.util.Scanner;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-/*		Scanner input = new Scanner(System.in);
-		double deposito=0;
-		System.out.print("Entre com um valor double");
-		deposito = Double.parseDouble(input.nextLine());
-		System.out.println(deposito);
-		*/
-		  SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+	public static void main(String[] args) throws ParseException { 
+	 
+	      try {
+	         Teste.receba();
+	      } catch (ParseException e) { 
+	    	  System.out.println("Data Inválida");
+	    	  Teste.receba();
+	         
+	      }
+	}
+	
+	public static void receba() throws ParseException{
+		  SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy");
 	      Scanner sc = new Scanner(System.in);
 	 
 	      System.out.println("Eample: 12-25-2103");
 	      System.out.print("Enter date: ");
 	      String str = sc.nextLine();
-	 
-	      try {
-	         Date date = sdf.parse(str); 
-	 
-	         sdf = new SimpleDateFormat("d/MMM/yyyy");
-	         System.out.println("Date: " + sdf.format(date));
-	      } catch (ParseException e) { 
-	         System.out.println("Entre com uma data válida");
-	      }
+		  Date date = sdf.parse(str); 
+		 
+         sdf = new SimpleDateFormat("d/mm/yyyy");
+         System.out.println("Date: " + sdf.format(date));
 	}
 
 }
