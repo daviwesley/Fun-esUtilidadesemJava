8a5c673ebfe50472388edc45da3d83a6b8b6facc
Davi Wesley <davi.wesley@icloud.com>
2016-12-10 00:11:08 -0200

    algorítimo melhorado by rbbr

--- scaneardata.java
+++ scaneardata.java
@@ -1,5 +1,3 @@
-package br.davi.teste;
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -9,27 +7,30 @@ import java.util.Scanner;
 public class Teste {
 
 	public static void main(String[] args) throws ParseException { 
-	 
-	      try {
-	         Teste.receba();
-	      } catch (ParseException e) { 
-	    	  System.out.println("Data Inválida");
-	    	  Teste.receba();
-	         
-	      }
+	     int a = 0;
+		 do{
+			 try {
+				 
+		         Teste.receba();
+		         a=0;
+		      } catch (ParseException e) { 
+		    	  System.out.println("Data Inválida");
+		    	  a=1;
+		      }
+		 }while(a != 0);	      
 	}
 	
 	public static void receba() throws ParseException{
-         SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy");
-	 Scanner sc = new Scanner(System.in);
+		 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/20yy");
+		 sdf.setLenient(false);
+	      Scanner sc = new Scanner(System.in);
 	 
-	 System.out.println("Eample: 12-25-2103");
-	 System.out.print("Enter date: ");
-	 String str = sc.nextLine();
-         Date date = sdf.parse(str); 
-		 
-         sdf = new SimpleDateFormat("d/mm/yyyy");
-         System.out.println("Date: " + sdf.format(date));
+	      System.out.println("Example: 12/05/2016");
+	      System.out.print("Digite a data: ");
+	      String str = sc.nextLine();
+		  Date date = sdf.parse(str); 
+	      //sdf = new SimpleDateFormat("dd/mm/yyyy");
+          
+          System.out.println("Date: " + sdf.format(date));
 	}
-
 }
